e6f5ae110cb896829db5b3b3111781cf1950687d
HammerM <marcell.hammer@gmail.com>
2023-04-21 12:59:48 +0200

    Mission model

--- B0L3FV_HFT_2022232.Models/Mission.cs
+++ B0L3FV_HFT_2022232.Models/Mission.cs
@@ -23,5 +23,29 @@ namespace B0L3FV_HFT_2022232.Models
         public string MType { get; set; }
         [StringLength(100)]
         public string Location { get; set; }
+        public int Kills { get; set; }
+        public int Loot { get; set; }
+        public int Deaths { get; set; }
+
+        public virtual Goblin Goblin { get; set; }
+        public Mission()
+        {
+
+        }
+        public Mission(string line)
+        {
+            string[] split = line.Split('*');
+            MissionID = int.Parse(split[0]);
+            Date = DateTime.Parse(split[1]);
+            GoblinID = int.Parse(split[2]);
+            Hazard = int.Parse(split[3]);
+            MissionCompleted = bool.Parse(split[4]);
+            MissionDuration = int.Parse(split[5]);
+            MType = split[6];
+            Location = split[7];
+            Kills = int.Parse(split[8]);
+            Deaths = int.Parse(split[9]);
+            Loot = int.Parse(split[10]);
+        }
     }
 }
