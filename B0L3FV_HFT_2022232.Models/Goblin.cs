5f6ccec3df002e8b774b3434dc29b76e8938df40
HammerM <marcell.hammer@gmail.com>
2023-04-21 12:54:24 +0200

    Model classes added

---
+++ B0L3FV_HFT_2022232.Models/Goblin.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace B0L3FV_HFT_2022232.Models
+{
+    public class Goblin
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int GoblinID { get; set; }
+    }
+}
