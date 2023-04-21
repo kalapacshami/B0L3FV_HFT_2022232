6bad6013bd12507839d07d7250e520243284b3fd
HammerM <marcell.hammer@gmail.com>
2023-04-21 12:55:23 +0200

    Goblin class edit

--- B0L3FV_HFT_2022232.Models/Goblin.cs
+++ B0L3FV_HFT_2022232.Models/Goblin.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
 
 namespace B0L3FV_HFT_2022232.Models
 {
@@ -9,5 +10,31 @@ namespace B0L3FV_HFT_2022232.Models
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int GoblinID { get; set; }
+
+        public int WID { get; set; }
+        [StringLength(140)]
+        public string GoblinName { get; set; }
+        [Range(0, 20)]
+        public int Level { get; set; }
+        public int Money { get; set; }
+        public float Height { get; set; }
+
+        public virtual ICollection<Mission> Missions { get; set; }
+        public virtual Work Work { get; set; }
+        public Goblin()
+        {
+
+        }
+        public Goblin(string line)
+        {
+            string[] split = line.Split('*');
+
+            GoblinID = int.Parse(split[0]);
+            WID = int.Parse(split[1]);
+            GoblinName = split[2];
+            Level = int.Parse(split[3]);
+            Money = int.Parse(split[4]);
+            Height = float.Parse(split[5]);
+        }
     }
 }
