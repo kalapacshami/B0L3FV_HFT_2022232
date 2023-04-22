9d1f867badc603b379a6defaaef3368f010452cb
HammerM <marcell.hammer@gmail.com>
2023-04-22 22:26:10 +0200

    goblin

--- B0L3FV_HFT_2022232.Repository/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace B0L3FV_HFT_2022232.Repository
-{
-    public class Class1
-    {
-    }
-}
---
+++ B0L3FV_HFT_2022232.Repository/GoblinDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Security.Cryptography;
+using System.Xml.Linq;
+using System.Data;
+using B0L3FV_HFT_2022232.Models;
+
+namespace B0L3FV_HFT_2022232.Repository
+{
+    public class GoblinDbContext : DbContext
+    {
+    }
+}
