d5d342aa6071bf569bffc46a48d0a195a1d1d5b6
HammerM <marcell.hammer@gmail.com>
2023-04-22 22:26:35 +0200

    asd

--- B0L3FV_HFT_2022232.Repository/GoblinDbContext.cs
+++ B0L3FV_HFT_2022232.Repository/GoblinDbContext.cs
@@ -9,5 +9,10 @@ namespace B0L3FV_HFT_2022232.Repository
 {
     public class GoblinDbContext : DbContext
     {
+        public DbSet<Goblin> Goblins { get; set; }
+
+        public DbSet<Work> Works { get; set; }
+
+        public DbSet<Mission> Missions { get; set; }
     }
 }
