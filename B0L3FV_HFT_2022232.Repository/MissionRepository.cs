c4f8c31fbc4867c0f54f96e21f7871c322c3079a
HammerM <marcell.hammer@gmail.com>
2023-04-23 19:52:44 +0200

    MissionRepo

--- B0L3FV_HFT_2022232.Repository/MissionRepository.cs
+++ B0L3FV_HFT_2022232.Repository/MissionRepository.cs
@@ -9,6 +9,18 @@ namespace B0L3FV_HFT_2022232.Repository
 {
     public class MissionRepository : Repository<Mission>, IRepository<Mission>
     {
+        public MissionRepository(GoblinDbContext ctx) : base(ctx)
+        {
+        }
 
+        public override Mission Read(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Update(Mission item)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
