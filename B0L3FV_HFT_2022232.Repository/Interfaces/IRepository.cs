1b12e4e51e30cbd2d05e66a8d0567daf56fabc6c
HammerM <marcell.hammer@gmail.com>
2023-04-23 18:17:08 +0200

    asd

---
+++ B0L3FV_HFT_2022232.Repository/Interfaces/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B0L3FV_HFT_2022232.Repository
+{
+    public interface IRepository<T> where T : class 
+    {
+        IQueryable<T> ReadAll();
+
+        T Read(int id);
+
+        void Create(T item);
+
+        void Update(T item);
+
+        void Delete(int id);
+    }
+}
---
+++ B0L3FV_HFT_2022232.Repository/Repository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B0L3FV_HFT_2022232.Repository
+{
+    public abstract class Repository<T> : IRepository<T> where T : class
+    {
+        public void Create(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Read(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<T> ReadAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
