b915bd7af2566b2b838accda09addafc8fa64e31
HammerM <marcell.hammer@gmail.com>
2023-04-23 19:59:37 +0200

    Model Repos

---
+++ B0L3FV_HFT_2022232.Repository/ModelRepositories/GoblinRepository.cs
@@ -0,0 +1,26 @@
+﻿using B0L3FV_HFT_2022232.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B0L3FV_HFT_2022232.Repository
+{
+    public class GoblinRepository : Repository<Goblin>, IRepository<Goblin>
+    {
+        public GoblinRepository(GoblinDbContext ctx) : base(ctx)
+        {
+        }
+
+        public override Goblin Read(int id)
+        {
+            return ctx.Goblins.FirstOrDefault(t => t.GoblinID == id);
+        }
+
+        public override void Update(Goblin item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ B0L3FV_HFT_2022232.Repository/ModelRepositories/WorkRepository.cs
@@ -0,0 +1,26 @@
+﻿using B0L3FV_HFT_2022232.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B0L3FV_HFT_2022232.Repository
+{
+    public class WorkRepository : Repository<Work>, IRepository<Work>
+    {
+        public WorkRepository(GoblinDbContext ctx) : base(ctx)
+        {
+        }
+
+        public override Work Read(int id)
+        {
+            return ctx.Works.FirstOrDefault(t => t.WID == id);
+        }
+
+        public override void Update(Work item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
