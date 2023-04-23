9942b4b6ac6f5f55ea22342c7b3518c48e09ec1a
HammerM <marcell.hammer@gmail.com>
2023-04-23 19:47:39 +0200

    repo done

--- B0L3FV_HFT_2022232.Repository/Repository.cs
+++ B0L3FV_HFT_2022232.Repository/Repository.cs
@@ -32,7 +32,7 @@ namespace B0L3FV_HFT_2022232.Repository
 
         public IQueryable<T> ReadAll()
         {
-            
+            return ctx.Set<T>();
         }
 
         public abstract void Update(T item);
