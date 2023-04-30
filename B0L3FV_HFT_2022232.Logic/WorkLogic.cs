3a5c0b586de631510643552a9eda901ee180d302
HammerM <marcell.hammer@gmail.com>
2023-04-30 14:03:13 +0200

    WorkLogic

---
+++ B0L3FV_HFT_2022232.Logic/WorkLogic.cs
@@ -0,0 +1,38 @@
+﻿using B0L3FV_HFT_2022232.Models;
+using B0L3FV_HFT_2022232.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B0L3FV_HFT_2022232.Logic
+{
+    internal class WorkLogic 
+    {
+        public void Create(Work item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Work Read(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<Work> ReadAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Work item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
