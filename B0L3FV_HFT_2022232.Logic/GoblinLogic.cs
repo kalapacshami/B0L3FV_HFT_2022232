c5443eaf501c120af88a959871cdd2038ce96c2c
HammerM <marcell.hammer@gmail.com>
2023-04-30 13:57:24 +0200

    Logic

---
+++ B0L3FV_HFT_2022232.Logic/GoblinLogic.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using B0L3FV_HFT_2022232.Models;
+using B0L3FV_HFT_2022232.Repository;
+
+namespace B0L3FV_HFT_2022232.Logic
+{
+    public class GoblinLogic 
+    {
+        public void Create(Goblin item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Goblin Read(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<Goblin> ReadAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Goblin item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
