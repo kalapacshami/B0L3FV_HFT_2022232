64aec602efeffa57ef4b08e432180e83899ab603
HammerM <marcell.hammer@gmail.com>
2023-11-14 17:45:22 +0100

    non-crud done WPF

--- B0L3FV_HFT_2022232.WpfClient/VM/KillCountWindowViewModel.cs
+++ B0L3FV_HFT_2022232.WpfClient/VM/KillCountWindowViewModel.cs
@@ -1,12 +1,67 @@
-﻿using System;
+﻿using B0L3FV_HFT_2022232.Models;
+using Microsoft.Toolkit.Mvvm.Input;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+using MovieDbApp.RestClient;
 
 namespace B0L3FV_HFT_2022232.WpfClient.VM
 {
-    class KillCountWindowViewModel
+    public class KillCountWindowViewModel: ObservableRecipient
     {
+        private string errorMessage;
+
+        public string ErrorMessage
+        {
+            get { return errorMessage; }
+            set { SetProperty(ref errorMessage, value); }
+        }
+        private string answer;
+        public string Answer
+        {
+            get
+            {
+                return answer;
+
+            }
+            set
+            {
+                SetProperty(ref answer, value);
+            }
+        }
+        public static bool IsInDesignMode
+        {
+            get
+            {
+                var prop = DesignerProperties.IsInDesignModeProperty;
+                return (bool)DependencyPropertyDescriptor.FromProperty(prop, typeof(FrameworkElement)).Metadata.DefaultValue;
+            }
+        }
+
+        public List<Tool5> KillMissions { get; set; }
+
+        public ICommand KillMissionCommand { get; set; }
+
+        public KillCountWindowViewModel()
+        {
+            if (!IsInDesignMode)
+            {
+                KillMissionCommand = new RelayCommand(() =>
+                {
+                    KillMissions = new RestService("http://localhost:11828/").Get<Tool5>("tool/KillCountMissions");
+
+                    foreach (var item in KillMissions)
+                    {
+                        Answer += "Goblin called: " + item.Name + ", Goblin's work: " + item.Goblin_work + " , Kills: " + item.Kill + " , ID: " + item.Id + Environment.NewLine;
+                    }
+                });
+            }
+        }
     }
 }
--- B0L3FV_HFT_2022232.WpfClient/VM/MissionStatusWindowViewModel.cs
+++ B0L3FV_HFT_2022232.WpfClient/VM/MissionStatusWindowViewModel.cs
@@ -1,12 +1,67 @@
-﻿using System;
+﻿using B0L3FV_HFT_2022232.Models;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows;
+using MovieDbApp.RestClient;
 
 namespace B0L3FV_HFT_2022232.WpfClient.VM
 {
-    class MissionStatusWindowViewModel
+    public class MissionStatusWindowViewModel : ObservableRecipient
     {
+        private string errorMessage;
+
+        public string ErrorMessage
+        {
+            get { return errorMessage; }
+            set { SetProperty(ref errorMessage, value); }
+        }
+        private string answer;
+        public string Answer
+        {
+            get
+            {
+                return answer;
+
+            }
+            set
+            {
+                SetProperty(ref answer, value);
+            }
+        }
+        public static bool IsInDesignMode
+        {
+            get
+            {
+                var prop = DesignerProperties.IsInDesignModeProperty;
+                return (bool)DependencyPropertyDescriptor.FromProperty(prop, typeof(FrameworkElement)).Metadata.DefaultValue;
+            }
+        }
+
+        public List<Tool2> StatusMissions { get; set; }
+
+        public ICommand StatusCommand { get; set; }
+
+        public MissionStatusWindowViewModel()
+        {
+            if (!IsInDesignMode)
+            {
+                StatusCommand = new RelayCommand(() =>
+                {
+                    StatusMissions = new RestService("http://localhost:11828/").Get<Tool2>("tool/Missions");
+
+                    foreach (var item in StatusMissions)
+                    {
+                        Answer += "Goblin's job: " + item.Name + ", Type: " + item.Type + " , Id: " + item.Id + Environment.NewLine;
+                    }
+                });
+            }
+        }
     }
 }
